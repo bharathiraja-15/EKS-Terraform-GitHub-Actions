93f9560dfdd6d4a60607a69ed2f22bf74340bae2
bharathiraja-15 <bharathimtech11@gmail.com>
2026-02-21 10:34:41 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -1,20 +1,22 @@
 terraform {
   required_version = "~> 1.9.3"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.49.0"
     }
   }
+
   backend "s3" {
     bucket         = "bharathiraja-tf-backend"
     region         = "eu-north-1"
     key            = "eks/terraform.tfstate"
     dynamodb_table = "bharathiraja-lock-files"
     encrypt        = true
-}
+  }
 }
 
 provider "aws" {
-  region = var.aws-region
+  region = var.aws_region
 }
